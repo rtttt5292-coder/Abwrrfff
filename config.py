0b0cac1da46e6ddb5f58cbbadd0e3953f198a03f
rtttt5292-coder <rtttt5292@gmail.com>
2026-05-21 07:36:54 +0300

    upload config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# ============================================================
+#Group Manager Bot
+# Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
+# Support: https://t.me/LearningBotsCommunity
+# Channel: https://t.me/learning_bots
+# YouTube: https://youtube.com/@learning_bots
+# License: Open-source (keep credits, no resale)
+# ============================================================
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Required configurations
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "")
+DB_NAME = os.getenv("DB_NAME", "Cluster0")
+
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+BOT_USERNAME = os.getenv("BOT_USERNAME", "NomadeHelpBot")
+
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/LearningBotsCommunity")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/Learning_Bots")
+START_IMAGE = os.getenv("START_IMAGE", "https://files.catbox.moe/o5eekb.jpg")
